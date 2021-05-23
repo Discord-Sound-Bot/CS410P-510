37577d8ea765d7cca3fe27aff32f6b499f2185c1
nof-pdx <82555229+nof-pdx@users.noreply.github.com>
2021-05-23 13:49:53 -0700

    Working voice recoginiton bot

--- main.py
+++ main.py
@@ -1,37 +1,99 @@
+import urllib
+from playsound import playsound
+import speech_recognition as sr
+from datetime import datetime
+import pyttsx3
+import random
+import pytz
 import os
-from discord.ext import commands
-
-client = commands.Bot(command_prefix='.')
-
-
-# client.remove_command("help")
+import urllib.request
+import re
+import youtube_dl
 
 
-@client.command()
-async def load(ctx, extension):
-    client.load_extension(f'cogs.{extension}')
+def ytsearch(song_name):
+    tts("searching for " + song_name)
+    song_name = song_name[5:]
+    search_keyword = song_name.replace(" ", "")
+    print(search_keyword)
+    html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
+    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    print("https://www.youtube.com/watch?v=" + video_ids[0])
+    downloader("https://www.youtube.com/watch?v=" + video_ids[0], song_name)
 
 
-@client.command()
-async def unload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
+def speech():
+    listener = sr.Recognizer()
+    try:
+        with sr.Microphone() as source:
+            print("listening...")
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            print(command)
+            return command
+    except:
+        pass
 
 
-@client.command()
-async def reload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-    client.load_extension(f'cogs.{extension}')
-    await ctx.send(f'The extension {extension} has been reloaded')
+def downloader(url, song_name):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        # 'quiet': True,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        print("Downloading audio now\n")
+        ydl.download([url])
+    play(song_name)
 
 
-@client.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.send("Invalid command used.")
+def play(song_name):
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            print(f"Renamed File: {file}\n")
+            os.rename(file, "song.mp3")
+    tts("playing " + song_name)
+    playsound("song.mp3")
+    os.remove("song.mp3")
+
+	
+def tts(string):
+    engine = pyttsx3.init()
+    engine.say(string)
+    engine.runAndWait()
 
 
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
+def time():
+    tz_NY = pytz.timezone('America/Los_Angeles')
+    datetime_NY = datetime.now(tz_NY)
+    print(datetime_NY.strftime("%I:%M %p"))
+    tts(datetime_NY.strftime("%I:%M %p"))
 
-client.run("ODQ1MTIyMzYwNjAwNDI4NTk1.YKcXiA.pp3FOBDgxCyykMPnCh3p56JmIKQ")
+
+def main():
+    quit = True
+    discord = False
+    while quit:
+        try:
+            phrase = speech()
+            if 'hey Discord' in phrase or discord:
+                if not discord:
+                    tts("Yes?")
+                discord = True
+                if 'play' in phrase:
+                    ytsearch(phrase)
+                if 'time' in phrase:
+                    time()
+                if 'quit' in phrase:
+                    tts("shutting down")
+                    quit = False
+        except:
+            pass
+
+
+if __name__ == "__main__":
+    main()
